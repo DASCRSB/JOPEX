aef127e303ffd24f3793b4ff7e506538739d44f5
Stephanie Dawsonn-Andoh <stephandoh@gmail.com>
2020-07-25 10:55:30 +0000

    Final work

--- Scripts/homework.sql
+++
@@ -1,5 +0,0 @@
-/* The aggregate function COUNT is used to find the number of customers wave has*/
-SELECT * FROM COUNT(u_id);
-
-/* The aggregate function COUNT is used to find the number of customers wave has*/
-SELECT * FROM COUNT(dest_wallet_id);
